90be1c25b28f89af80b3485adf2af60c0c6597ce
WebCodeLion <30550565+WebCodeLion@users.noreply.github.com>
2020-05-20 19:45:57 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+  const menuToggle = document.querySelector('#menu__togle');
+  const mobileNavContainer = document.querySelector('#mobile__nav');
+
+  menuToggle.onclick = function(){
+      menuToggle.classList.toggle('menu-icon-active');
+      mobileNavContainer.classList.toggle('mobile-nav--active');
+  }
+
+  
+/////////////////////////////////
+
+
+
+ 
